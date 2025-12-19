0daab190ff0bcc13c5de3b70f546aead764b3698
Bryant <bryant.mai@changiairport.com>
2025-12-19 16:40:30 +0700

    [feature] HelloController: add method endpont bye

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -6,8 +6,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping
+    @GetMapping("/hello")
     public String hello() {
         return "Hello world";
     }
+
+    @GetMapping("/bye")
+    public String Bye() {
+        return "Good bye everyone";
+    }
+
 }
